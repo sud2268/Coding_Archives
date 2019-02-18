cd7cc226d9851e8869cd3e805c93c54db43db991
sudhanshu <sudhanshu2268@gmail.com>
2019-02-18 15:20:17 +0530

    Graph implementation

---
+++ src/Graphs/Client.java
@@ -0,0 +1,37 @@
+package Graphs;
+
+import Graphs.Graph;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Graph graph=new Graph();
+		graph.addVertex("a");
+		graph.addVertex("b");
+		graph.addVertex("c");
+		graph.addVertex("d");
+		graph.addVertex("e");
+		graph.addVertex("f");
+		graph.addVertex("g");
+		graph.addEdge("a", "b", 2);
+		graph.addEdge("a", "d", 3);
+		graph.addEdge("b", "c", 4);
+		graph.addEdge("c", "d", 5);
+		graph.addEdge("d", "e", 6);
+		System.out.println(graph.containsKey("c"));
+		System.out.println(graph.containsEdge("a", "b"));
+		System.out.println();
+		graph.display();
+		System.out.println("....................");
+		graph.removeVertex("f");
+		graph.display();
+		System.out.println("....................");
+
+		graph.removeVertex("a");
+		graph.display();
+		
+		
+	}
+
+}
---
+++ src/Graphs/Graph.java
@@ -0,0 +1,115 @@
+package Graphs;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Graph {
+	
+	//Main hashmap vertices
+	HashMap<String , Vertex> vertices; 
+	
+	//Vertex object
+	private class Vertex{
+		HashMap<String , Integer> nbrs=new HashMap<>();
+	}
+	
+	//constructor of the graph class
+	public Graph() {
+		vertices=new HashMap<>();
+	}
+	
+	//check if contains key 
+	public boolean containsKey(String vname) {
+		return this.vertices.containsKey(vname);
+	}
+	
+	
+	//count number of vertex
+	public int countVertex() {
+		return this.vertices.size();
+	}
+	
+	//add vertex to graph
+	public void addVertex(String Vname) {
+		Vertex vtx=new Vertex();
+		vertices.put(Vname, vtx);
+	}
+	
+	//count number of edges
+	public void numEdges() {
+		
+		ArrayList<Vertex> keys=new ArrayList<>();
+		int count=0;
+		for (Vertex key : keys) {
+			count+=key.nbrs.size();
+		}
+	}
+	
+	
+	//contains edge
+	public boolean containsEdge(String vname1,String vname2) {
+		
+		Vertex vtx1=vertices.get(vname1);
+		Vertex vtx2=vertices.get(vname2);
+		
+		if(vtx1==null || vtx2==null || !vtx1.nbrs.containsKey(vname2)) {
+			return false;
+		}
+		return true;
+	}
+	
+	//add edge
+	public void addEdge(String vname1 ,String vname2,int cost) {
+		Vertex vtx1=vertices.get(vname1);
+		Vertex vtx2=vertices.get(vname2);
+		
+		if(vtx1==null || vtx2==null || vtx1.nbrs.containsKey(vname2)) {
+			return;
+		}
+		vtx1.nbrs.put(vname2, cost);
+		vtx2.nbrs.put(vname1, cost);
+	}
+	
+	
+	//remove Edge
+	public void removeEdge(String vname1 ,String vname2,int cost) {
+		Vertex vtx1=vertices.get(vname1);
+		Vertex vtx2=vertices.get(vname2);
+		
+		if(vtx1==null || vtx2==null || !vtx1.nbrs.containsKey(vname2)) {
+			return;
+		}
+		vtx1.nbrs.remove(vname2);
+		vtx2.nbrs.remove(vname1);
+	}
+	
+	//remove Vertex
+	public void removeVertex(String vname) {
+		
+		//get the object of Type vertex at the 
+		Vertex vtx=vertices.get(vname);
+		
+		ArrayList<String > keys =new ArrayList<>(vtx.nbrs.keySet());
+		for (String key : keys) {
+			//object of type vertex is created to store the object at that key
+			Vertex nbrvertex=vertices.get(key);
+			nbrvertex.nbrs.remove(vname);
+		}
+		vertices.remove(vname);
+		
+	}
+	public void display() {
+		//create an arraylist of all the keys present in the vertices hashmap
+		ArrayList<String> keys=new ArrayList<>(vertices.keySet());
+		
+		//Traverse over keys
+		for (String key : keys) {
+			
+			//create an object vertex to get the object at that key 
+			Vertex vertex=vertices.get(key);
+			
+			//print the neighbours hashmap of the vertex
+			System.out.println(key +"\t" +vertex.nbrs);
+		}
+	}
+	
+}
